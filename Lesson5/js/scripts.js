94bacc3ca98ddaed273f1a67dc4b37cf40bd62c6
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-02-05 14:04:54 -0600

    changes

--- Lesson5/js/scripts.js
+++ Lesson5/js/scripts.js
@@ -2,3 +2,18 @@ function toggleMenu() {
 
     document.getElementById("primaryNav").classList.toggle("hide");
 }
+var currentDate = new Date();
+var dayNames = ["Sunday","Monday", "Tuesday", "Wednesday", 
+"Thursday", "Friday", "Saturday"];
+var day = new Date().getDay();
+document.getElementById("day1").innerText = dayNames[(day + 1) % 7];
+document.getElementById("day2").innerText = dayNames[(day + 2) % 7];
+document.getElementById("day3").innerText = dayNames[(day + 3) % 7];
+document.getElementById("day4").innerText = dayNames[(day + 4) % 7];
+document.getElementById("day5").innerText = dayNames[(day + 5) % 7];
+if (day != 5) {
+    document.getElementById("alert").style.display = "none";
+}
+else{
+    document.display="Today is pizza day";
+}

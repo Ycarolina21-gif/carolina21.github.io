7b2e5ae5d85e0fa241d3e3323e9a5bf048f0c742
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-02-09 15:05:34 -0500

    changes

--- Lesson5/js/scripts.js
+++ Lesson5/js/scripts.js
@@ -6,5 +6,5 @@ var currentDate = new Date();
 var now = new Date();
 var dayOfWeek = now.getDay();
 if (dayOfWeek != 5) {
-    document.getElementById("announcement").style.display = "none";
+    document.getElementById("alert").style.display = "none";
 }

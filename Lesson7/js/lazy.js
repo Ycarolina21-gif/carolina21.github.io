f3375dddd514c2f2ae4b9fb4ea1ac6791eb0dcea
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-02-19 20:13:06 -0500

    changes

---
+++ Lesson7/js/lazy.js
@@ -0,0 +1,28 @@
+const images =document.querySelectorAll("img[data-src]")
+function preloadImages(img){
+    const src= img.getAttribute("data-src");
+    if(!src){
+        return;
+    }
+    img.src=src;
+}
+
+
+const imgOptions={
+    threshold: 1,
+    rootMargin: "0px 0px 300px 0px"
+};
+const imgObserver=new IntersectionObserver((entries,imgObserver) => {
+   entries.forEach(entry =>{
+       if (!entry.isIntersecting){
+           return;
+       } else{
+           preloadImages(entry.target);
+           imgObserver.unobserve(entry.target);
+       }
+   }) 
+},imgOptions)
+
+images.forEach(images =>{
+    imgObserver.observe(images);
+});

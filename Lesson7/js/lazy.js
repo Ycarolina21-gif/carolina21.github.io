9f6a8c8dc63e2bd4a53f8dd009c1f978a5f00015
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-02-22 09:49:14 -0500

    changes

--- Lesson7/js/lazy.js
+++ Lesson7/js/lazy.js
@@ -1,8 +1,8 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
-const imgOptions={
+/*const imgOptions={
     threshold:0,
     rootMargin:"0px 0px 50px 0px"
-};
+};*/
 const loadImages = (image) => {
     image.setAttribute('src', image.getAttribute('data-src'));
     image.onload = () => {

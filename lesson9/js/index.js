38c24cf45b513f3b51185046bd1f520bd9541a4d
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-03-07 11:14:28 -0500

    changes

--- lesson9/js/index.js
+++ lesson9/js/index.js
@@ -10,41 +10,43 @@ fetch(requestURL)
     for (let i = 0; i < towns.length; i++ ) {
       if (towns[i].name == "Fish Haven" || towns[i].name == "Preston" ||  towns[i].name== "Soda Springs") {
         let card = document.createElement('section');
-        
-      let h2 = document.createElement('h1');
+        card.setAttribute('class', 'townsection');
+        let carddiv = document.createElement('div')
+        card.appendChild(carddiv);
+      let h2 = document.createElement('h2');
+      h2.setAttribute('class', 'townname');
         h2.textContent = towns[i].name;
-        card.appendChild(h2);
+        carddiv.appendChild(h2);
  
-   
-   let motto  = document.createElement('h2');
-   motto.textContent = towns[i].motto;
-        card.appendChild(motto);
-        
+        let motto  = document.createElement('h3');
+      motto.setAttribute('class', 'townmotto');
+      motto.textContent = towns[i].motto;
+            carddiv.appendChild(motto);
+            
        let yearFounded  = document.createElement('p');
+       yearFounded.setAttribute('class', 'towninfo');
         yearFounded.textContent = "Year Founded: "  + towns[i].yearFounded;
-        card.appendChild(yearFounded);
+        carddiv.appendChild(yearFounded);
         
         let currentPopulation = document.createElement('p');
+        currentPopulation.setAttribute('class', 'towninfo');
         currentPopulation.textContent = "Current Population: " + towns[i].currentPopulation;
-        card.appendChild(currentPopulation);
+        carddiv.appendChild(currentPopulation);
 
         let averageRainfall = document.createElement ('p');
+        averageRainfall.setAttribute('class', 'towninfo');
         averageRainfall.textContent = "Average RainFall: "  + towns[i].averageRainfall;
-        card.appendChild(averageRainfall);
+        carddiv.appendChild(averageRainfall);
 
-        let myImg = document.createElement('images');
-        if (towns[i].name === "Fish Haven"){
-          myImg.src='images/fishhaven.jpg';
-      } else if (towns[i].name === "Preston") {
-          myImg.src='images/preston.jpg';
-      } else {
-          myImg.src='images/sodasprings.jpg';
-      }
-      
+        let image = document.createElement('img');
+        image.setAttribute('class', 'townphoto');
+        image.setAttribute('src', "images/" + towns[i].photo);
+        image.setAttribute('alt', "picture of: " + h2.textContent);
+       card.appendChild(image);
 
         document.querySelector('div.cards').appendChild(card);
 
   }}}); 
  ;
 
-  
+ 

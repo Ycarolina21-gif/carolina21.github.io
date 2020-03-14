67236207713efd515742f731d57ea03c512e880c
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-03-14 11:19:09 -0400

    changes

--- Lesson10/js/weatherapi.js
+++ Lesson10/js/weatherapi.js
@@ -1,4 +1,4 @@
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=3014d71ceff446035940314c162afafa";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=3014d71ceff446035940314c162afafa";
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {

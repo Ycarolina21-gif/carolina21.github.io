b914166df7419a675d5e0c2792516292c6283066
Ycarolina21-gif <Ycarolina21@byui.edu>
2020-03-30 21:09:05 -0400

    changes

--- Final/js/people.js
+++ Final/js/people.js
@@ -6,34 +6,32 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);  // temporary checking for valid response and data parsing
-    const People = jsonObject['Activity'];
+    const People = jsonObject['People'];
     for (let i = 0; i < People.length; i++ ) {
       if (People[i].name == "John Carson" || People[i].name == "Steve Smith"||  People[i].name == "David Olson") {
         let card = document.createElement('section');
-        card.setAttribute('class', 'Activitysection');
+        card.setAttribute('class', 'Peoplesection');
         
-        let image = document.createElement('img');
-        image.setAttribute('class', 'Peoplephoto');
-        image.setAttribute('src', "images/" + People[i].photo);
-       card.appendChild(image);
-       document.querySelector('div.cards').appendChild(card);
+        let photo = document.createElement('img');
+        photo.setAttribute('class', 'Peoplephoto');
+        photo.setAttribute('src', "images/" + People[i].photo);
+       card.appendChild(photo);
+       document.querySelector('div.blog').appendChild(card);
         
         let carddiv = document.createElement('div')
+        carddiv.setAttribute('class', 'Description');
         card.appendChild(carddiv);
+        
      
         let h2 = document.createElement('h2');
       h2.setAttribute('class', 'Peoplename');
         h2.textContent = People[i].name;
         carddiv.appendChild(h2);
         
-        let Certification  = document.createElement('p');
-        Certification.setAttribute('class', 'ActivitySkillLevel');
-        Certification.textContent = "Certifications: " + People[i].Certification;
-            carddiv.appendChild(Certification);
             
        let Years  = document.createElement('p');
-       Years.setAttribute('class', 'ActivityLength');
-       Length.textContent = "Years of experience: "  + People[i].Years + " years";
+       Years.setAttribute('class', 'Years');
+       Years.textContent = "Years of experience: "  + People[i].Years + " years";
         carddiv.appendChild(Years);
         
         let Email = document.createElement('p');
@@ -41,12 +39,17 @@ fetch(requestURL)
         Email.textContent = People[i].Email;
         carddiv.appendChild(Email);
 
+        let Certification  = document.createElement('p');
+        Certification.setAttribute('class', 'Certification');
+        Certification.textContent = "Certifications: " + People[i].Certification;
+            carddiv.appendChild(Certification);
+
         let Brief = document.createElement ('p');
         Brief.setAttribute('class', 'Brief');
         Brief.textContent = "A little about me: "  + People[i].Brief ;
         carddiv.appendChild(Brief);
 
-        document.querySelector('div.cards').appendChild(card);
+        document.querySelector('div.blog').appendChild(card);
 
   }}}); 
  ;
